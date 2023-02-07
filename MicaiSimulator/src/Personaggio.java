3b7fd915488f955837823767ec5c106d49f9034f
Astora <hotelebola@gmail.com>
2023-02-07 20:06:07 +0100

    Rico

--- MicaiSimulator/src/Personaggio.java
+++ MicaiSimulator/src/Personaggio.java
@@ -1,3 +1,14 @@
 public class Personaggio {
+    private int vita = 100;
+    private float speed= 100;
 
+
+
+    public float getSpeed() {
+        return speed;
+    }
+
+    public int getVita() {
+        return vita;
+    }
 }
